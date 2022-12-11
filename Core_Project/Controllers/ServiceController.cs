dd42d0716143224e4171ec0fb94b1e4b88029c17
mustafa barlas <mustafa.barlas1146@gmail.com>
2022-12-12 01:19:30 +0300

    new layout added

--- Core_Project/Controllers/ServiceController.cs
+++ Core_Project/Controllers/ServiceController.cs
@@ -1,11 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Project.Controllers
 {
     public class ServiceController : Controller
     {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+
         public IActionResult Index()
         {
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+
+        public IActionResult DeleteService(int id)
+        {
+            var values = serviceManager.TGetByID(id);
+            serviceManager.Tdelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AddService() {
+
+           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddService(Service service)
+        {
+            ServiceValidator validations = new ServiceValidator();
+            ValidationResult results = validations.Validate(service);
+            if (results.IsValid)
+            {
+                serviceManager.Tadd(service);
+                return RedirectToAction("Index");   
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EditService(int id)
+        {
+            var values = serviceManager.TGetByID(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditService(Service service)
+        {
+            ServiceValidator validations = new ServiceValidator();
+            ValidationResult results = validations.Validate(service);
+            if (results.IsValid)
+            {
+                serviceManager.Tupdate(service);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
             return View();
         }
     }
