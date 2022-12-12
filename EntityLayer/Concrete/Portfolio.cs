152ac44b960501ef61e5e75b1c8763c008952134
mustafa barlas <mustafa.barlas1146@gmail.com>
2022-12-13 00:12:23 +0300

    New Template has been performed

--- EntityLayer/Concrete/Portfolio.cs
+++ EntityLayer/Concrete/Portfolio.cs
@@ -28,12 +28,5 @@ namespace EntityLayer.Concrete
 
         public string Value { get; set; }
 
-        public string Image1 { get; set; }
-
-        public string Image2 { get; set; }
-
-        public string Image3 { get; set; }
-
-        public string Image4 { get; set; }
     }
 }
