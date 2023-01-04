b02bdd30699c7f70abf5cd3aeb7b6e01c5e331e9
mustafa barlas <mustafa.barlas1146@gmail.com>
2023-01-04 22:44:22 +0300

    some

---
+++ Core_Project/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class ContactController : Controller
+    {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
+
+        public IActionResult Index()
+        {
+            var values = messageManager.TGetList();
+            return View(values);
+        }
+    }
+}
