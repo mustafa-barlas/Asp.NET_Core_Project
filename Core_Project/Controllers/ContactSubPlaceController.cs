354a692459161ef1b22f923e98e746d3b28ade2c
mustafa barlas <mustafa.barlas1146@gmail.com>
2023-01-06 02:56:23 +0300

    new things have been tried

---
+++ Core_Project/Controllers/ContactSubPlaceController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class ContactSubPlaceController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        
+        [HttpGet]
+        public IActionResult GetDetails()
+        {
+
+            var values = contactManager.TGetByID(1);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult GetDetails(Contact contact)
+        {
+            
+            contactManager.Tupdate(contact);
+            return RedirectToAction("Index","Dashboard");
+        }
+
+    }
+}
---
+++ Core_Project/Controllers/TestimonialController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Project.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+
+        public IActionResult Index()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            testimonialManager.Tdelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditTestimonial(Testimonial testimonial)
+        {
+            testimonialManager.Tupdate(testimonial);
+            return RedirectToAction("Index");
+        }
+    }
+}
